3724442555ad7191c87f671ad31fbfe28e224534
Chewby Irinine <al_kor_00@bk.ru>
2022-11-02 00:31:20 +0300

    Получение данных о валютах в разделе "Курс рубля"

--- pages/exchange-rates/index.vue
+++ pages/exchange-rates/index.vue
@@ -3,41 +3,28 @@
     <main class="px-4 py-5">
       <h1 class="mb-5 font-bold text-20 text-primary">Курс рубля</h1>
       <section class="flex flex-col gap-5 mb-4">
-        <article class="p-6 pb-10 bg-gray rounded-2xl">
+        <article
+          v-for="(currency, index) in getCurrencies"
+          :key="currency.ID"
+          class="p-6 pb-10 bg-gray rounded-2xl"
+        >
           <img
             class="mb-5"
-            src="@/assets/svg/currency-blue.svg"
-            alt="currency-blue"
+            :src="`/_nuxt/assets/svg/currency-${getIconColor(index)}.svg`"
+            :alt="`currency-${getIconColor(index)}`"
           />
-          <h6 class="mb-5 font-bold text-17 text-primary">AUD</h6>
-          <p class="mb-7 text-14 text-text-gray-950">Австралийский доллар</p>
+          <h6 class="mb-5 font-bold text-17 text-primary">
+            {{ currency.CharCode }}
+          </h6>
+          <p class="mb-7 text-14 text-text-gray-950">{{ currency.Name }}</p>
           <ul class="space-y-4">
             <li class="flex gap-2 font-semibold text-14 text-text-gray-900">
               <img src="@/assets/svg/mark.svg" alt="mark" />
-              Номинал: 1
+              Номинал: {{ currency.Nominal }}
             </li>
             <li class="flex gap-2 font-semibold text-14 text-text-gray-900">
               <img src="@/assets/svg/mark.svg" alt="mark" />
-              Курс: 39.841
-            </li>
-          </ul>
-        </article>
-        <article class="p-6 pb-10 bg-gray rounded-2xl">
-          <img
-            class="mb-5"
-            src="@/assets/svg/currency-red.svg"
-            alt="currency-red"
-          />
-          <h6 class="mb-5 font-bold text-17 text-primary">AUD</h6>
-          <p class="mb-7 text-14 text-text-gray-950">Австралийский доллар</p>
-          <ul class="space-y-4">
-            <li class="flex gap-2 font-semibold text-14 text-text-gray-900">
-              <img src="@/assets/svg/mark.svg" alt="mark" />
-              Номинал: 1
-            </li>
-            <li class="flex gap-2 font-semibold text-14 text-text-gray-900">
-              <img src="@/assets/svg/mark.svg" alt="mark" />
-              Курс: 39.841
+              Курс: {{ currency.Value }}
             </li>
           </ul>
         </article>
@@ -56,5 +43,22 @@
 <script>
 export default {
   name: "ExchangeRates",
+
+  computed: {
+    getCurrencies() {
+      // Convert object to array to get index
+      return Object.values(this.$store.state.currencies.currencies);
+    },
+  },
+
+  created() {
+    this.$store.dispatch("currencies/GET_CURRENCIES");
+  },
+
+  methods: {
+    getIconColor(index) {
+      return index % 2 ? "red" : "blue";
+    },
+  },
 };
 </script>
